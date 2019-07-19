9f0168d5e5ef0a8c6b1db1481f6203d9491375ac
Khaki Huang <jiaqi228@gmail.com>
2019-07-19 19:52:57 +0800

    ComponentState demo.

--- App.js
+++ App.js
@@ -1,64 +1,41 @@
 import React, { Component } from 'react'
-import { View, SafeAreaView, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native'
 
-import Toggle from './Toggle'
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
 
 export default class App extends Component {
-
   state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    todos: ['Click to remove', 'Learn React Native', 'Write code', 'Ship App'],
+  }
+
+  onAddTodo = (text) => {
+    const {todos} = this.state
+    this.setState({
+      todos: [text, ...todos]
+    })
+  }
+
+  onRemoveTodo = (index) => {
+    const {todos} = this.state
+    this.setState({
+      todos: todos.filter((todo,i) => i!=index)
+    })
   }
 
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state  // Destructuring from state.
-    const layoutStyle = {flexDirection, justifyContent, alignItems} // Compositing state's details into a class called layoutStyle.
+    const {todos} = this.state
 
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <Toggle
-          label={'Primary axis (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']} // Hard-coding here.
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distribution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']} // Hard-coding here.
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']} // Hard-coding here.
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
+    return(
+      <SafeAreaView>
+        <Title>
+          To-Do List
+        </Title>
+        <Input placeholder={'Tyle a todo, then hit enter!'} onSubmitEditing={this.onAddTodo} />
+        <List list={todos} onPressItem={this.onRemoveTodo}/>
       </SafeAreaView>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
-  },
-})

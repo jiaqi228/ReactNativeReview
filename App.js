7882b4c4d4818d40f8baae7a2c7c740c869929d2
Khaki Huang <jiaqi228@gmail.com>
2019-07-19 18:57:50 +0800

    Flexbox demo.

--- App.js
+++ App.js
@@ -1,44 +1,64 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, SafeAreaView, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Counter color={'lightblue'} size={16} />
-      <Counter color={'skyblue'} size={32} />
-      <Counter color={'steelblue'} size={80} />
-      <Counter color={'darkblue'} size={140} />
-    </View>
-  );
-}
+import Toggle from './Toggle'
 
-class Counter extends Component { // Should be extended to Component to be a Component.
-  state = {count: 0}  // Definiation of State.
-  componentDidMount() { // componentDidMount lifecycle.
-    setInterval(() => {
-      this.setState({count: this.state.count + 1})
-    }, 1000)  // Update this component's state every 1 second.
+export default class App extends Component {
+
+  state = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 
-  render() {  // render() is the method to be called to present how this component looks like.
-    const {count} = this.state // extract count from state.
-    const {color, size} = this.props  // extract color and size from props. Props are passed by the component which using this component.
+  render() {
+    const {flexDirection, alignItems, justifyContent} = this.state  // Destructuring from state.
+    const layoutStyle = {flexDirection, justifyContent, alignItems} // Compositing state's details into a class called layoutStyle.
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
 
     return (
-      <Text style={{color, fontSize: size}}>
-        {count}
-      </Text>
+      <SafeAreaView style={styles.container}>
+        <Toggle
+          label={'Primary axis (flexDirection)'}
+          value={flexDirection}
+          options={['row', 'column']} // Hard-coding here.
+          onChange={(option) => this.setState({flexDirection: option})}
+        />
+        <Toggle
+          label={primaryAxis + ' distribution (justifyContent)'}
+          value={justifyContent}
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']} // Hard-coding here.
+          onChange={(option) => this.setState({justifyContent: option})}
+        />
+        <Toggle
+          label={secondaryAxis + ' alignment (alignItems)'}
+          value={alignItems}
+          options={['flex-start', 'center', 'flex-end', 'stretch']} // Hard-coding here.
+          onChange={(option) => this.setState({alignItems: option})}
+        />
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box} />
+          <View style={styles.box} />
+          <View style={styles.box} />
+        </View>
+      </SafeAreaView>
     )
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
-
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+  box: {
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5,
+  },
+})
